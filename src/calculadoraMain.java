65c8cd48e80a77764d9b5945859134c44b5c8dc1
cosesdehacker <nasrodo@gmail.com>
2018-03-29 19:14:04 +0200

    now?

---
+++ src/calculadoraMain.java
@@ -0,0 +1,10 @@
+public class calculadoraMain {
+
+    public static void main(String[] args) {
+
+        calculadora joc = new calculadora();
+        joc.calcula();
+
+        }
+    }
+
---
+++ src/exerciciNumeroAlQuadrat.java
@@ -0,0 +1,6 @@
+public class exerciciNumeroAlQuadrat {
+
+    public static void main(String[] args) {
+        
+    }
+}
